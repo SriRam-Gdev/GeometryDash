71d5758f6bad4d773fa5d551213b044116a68fb0
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2024-11-10 21:50:10 +0530

    initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forward = 2000f;
+    public float sideways = 500f;
+   
+    void Update()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, forward * Time.deltaTime);
+        }
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -forward * Time.deltaTime);
+        }
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sideways * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sideways * Time.deltaTime, 0, 0);
+        }
+    }
+}
